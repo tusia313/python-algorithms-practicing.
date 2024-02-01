fe0d53be56cede163b8cd68570969fad2e21ce5e
marta <loskotmarta@gmail.com>
2024-02-01 19:12:11 +0100

    Trudne kolejne ćiwczenie

--- silnia.py
+++ silnia.py
@@ -14,10 +14,18 @@
 
 # print(sum([2,4,6]))
 
-def count(lst):
-        if lst == []:
-            return 0
-        else:
-            return 1 + count(lst[1:])
+# def count(lst):
+#         if lst == []:
+#             return 0
+#         else:
+#             return 1 + count(lst[1:])
 
-print(count([1,3,4,9,111]))
+# print(count([1,3,4,9,111]))
+
+def max(number):
+    if len(number) == 2:
+        return number[0] if number[0] > number[1] else number[1]
+    sub_max = max(number[1:])
+    return number[0] if number[0] > sub_max else sub_max
+
+print(max([1,223,89,76]))
