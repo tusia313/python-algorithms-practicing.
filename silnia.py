093b6397227e46822a6a4ca1c8071092d925c2f5
marta <loskotmarta@gmail.com>
2024-02-01 19:05:10 +0100

    Add another exe

--- silnia.py
+++ silnia.py
@@ -6,10 +6,18 @@
 
 # print(fact(4))
 
-def sum(arr):
-    if arr == []:
-        return 0
-    else:
-        return arr[0] + sum(arr[1:])
+# def sum(arr):
+#     if arr == []:
+#         return 0
+#     else:
+#         return arr[0] + sum(arr[1:])
 
-print(sum([2,4,6]))
+# print(sum([2,4,6]))
+
+def count(lst):
+        if lst == []:
+            return 0
+        else:
+            return 1 + count(lst[1:])
+
+print(count([1,3,4,9,111]))
