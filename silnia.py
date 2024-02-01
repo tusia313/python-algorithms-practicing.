c55d9e10f2000efde3d7f90e10c8437501625169
marta <loskotmarta@gmail.com>
2024-02-01 18:56:36 +0100

    Add other exe

--- silnia.py
+++ silnia.py
@@ -1,7 +1,15 @@
-def fact(x):
-    if x == 1:
-        return 1
-    else:
-        return x * fact(x-1)
+# def fact(x):
+#     if x == 1:
+#         return 1
+#     else:
+#         return x * fact(x-1)
 
-print(fact(4))
+# print(fact(4))
+
+def sum(arr):
+    if arr == []:
+        return 0
+    else:
+        return arr[0] + sum(arr[1:])
+
+print(sum([2,4,6]))
